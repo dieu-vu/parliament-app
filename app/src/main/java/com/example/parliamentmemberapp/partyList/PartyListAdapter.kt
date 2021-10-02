4d3905f3a8ab2c2f0ac3fd41d86b5d03fcac7219
Dieu Vu <dieu.vu@metropolia.fi>
2021-10-02 22:10:27 +0300

    Refactored PartyList Adapter ViewHolder

--- app/src/main/java/com/example/parliamentmemberapp/partyList/PartyListAdapter.kt
+++ app/src/main/java/com/example/parliamentmemberapp/partyList/PartyListAdapter.kt
@@ -7,8 +7,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parliamentmemberapp.R
-import com.example.parliamentmemberapp.data.MemberOfParliament
-import com.example.parliamentmemberapp.databinding.TextItemViewBinding
 
 
 class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
@@ -26,34 +24,7 @@ class PartyListAdapter: RecyclerView.Adapter<PartyListAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-
-        holder.partyName.text = when (item){
-            "kd" -> "Christian Democrats"
-            "kesk" -> "Centre Party"
-            "kok" -> "National Coalition Party"
-            "liik" ->  "Movement Now"
-            "ps" -> "Finns party"
-            "r" -> "Swedish People's Party"
-            "sd" -> "Social Democratic Party"
-            "vas" -> "Left Alliance"
-            "vihr" -> "Green League"
-            else -> ""
-        }
-
-        holder.partyLogo.setImageResource( when(item) {
-            "kd" -> R.drawable.kd
-            "kesk" -> R.drawable.kesk
-            "kok" -> R.drawable.kok
-            "liik" ->  R.drawable.liik
-            "ps" -> R.drawable.ps
-            "r" -> R.drawable.rkp
-            "sd" -> R.drawable.sdp
-            "vas" -> R.drawable.vas
-            "vihr" -> R.drawable.vihr
-            else -> R.drawable.ic_launcher_foreground
-        })
-
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -66,6 +37,39 @@ class PartyListAdapter: RecyclerView.Adapter<PartyListAdapter.ViewHolder>() {
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val partyName: TextView = itemView.findViewById(R.id.party_name)
         val partyLogo: ImageView = itemView.findViewById(R.id.party_logo)
+    // Encapsulate binding on ViewHolder
+        fun bind(
+            item: String
+        ) {
+            val res = itemView.context.resources
+            partyName.text = when (item) {
+                "kd" -> "Christian Democrats"
+                "kesk" -> "Centre Party"
+                "kok" -> "National Coalition Party"
+                "liik" -> "Movement Now"
+                "ps" -> "Finns party"
+                "r" -> "Swedish People's Party"
+                "sd" -> "Social Democratic Party"
+                "vas" -> "Left Alliance"
+                "vihr" -> "Green League"
+                else -> ""
+            }
+
+            partyLogo.setImageResource(
+                when (item) {
+                    "kd" -> R.drawable.kd
+                    "kesk" -> R.drawable.kesk
+                    "kok" -> R.drawable.kok
+                    "liik" -> R.drawable.liik
+                    "ps" -> R.drawable.ps
+                    "r" -> R.drawable.rkp
+                    "sd" -> R.drawable.sdp
+                    "vas" -> R.drawable.vas
+                    "vihr" -> R.drawable.vihr
+                    else -> R.drawable.ic_launcher_foreground
+                }
+            )
+        }
     }
 }
 
