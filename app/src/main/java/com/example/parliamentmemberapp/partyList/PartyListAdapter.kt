99ec7e319ce995670aa076570cf6d4b1ae35e7a6
Dieu Vu <dieu.vu@metropolia.fi>
2021-10-02 21:56:22 +0300

    set list item party list in Recycler View

--- app/src/main/java/com/example/parliamentmemberapp/partyList/PartyListAdapter.kt
+++ app/src/main/java/com/example/parliamentmemberapp/partyList/PartyListAdapter.kt
@@ -1,34 +1,73 @@
 package com.example.parliamentmemberapp.partyList
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parliamentmemberapp.R
 import com.example.parliamentmemberapp.data.MemberOfParliament
 import com.example.parliamentmemberapp.databinding.TextItemViewBinding
 
+
 class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
 
-class PartyListAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+
+class PartyListAdapter: RecyclerView.Adapter<PartyListAdapter.ViewHolder>() {
+
     var data = listOf<String>()
     set(value) {
         field = value
         notifyDataSetChanged() // Tell Recycler not to draw old items
     }
+
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item
+        val res = holder.itemView.context.resources
+
+        holder.partyName.text = when (item){
+            "kd" -> "Christian Democrats"
+            "kesk" -> "Centre Party"
+            "kok" -> "National Coalition Party"
+            "liik" ->  "Movement Now"
+            "ps" -> "Finns party"
+            "r" -> "Swedish People's Party"
+            "sd" -> "Social Democratic Party"
+            "vas" -> "Left Alliance"
+            "vihr" -> "Green League"
+            else -> ""
+        }
+
+        holder.partyLogo.setImageResource( when(item) {
+            "kd" -> R.drawable.kd
+            "kesk" -> R.drawable.kesk
+            "kok" -> R.drawable.kok
+            "liik" ->  R.drawable.liik
+            "ps" -> R.drawable.ps
+            "r" -> R.drawable.rkp
+            "sd" -> R.drawable.sdp
+            "vas" -> R.drawable.vas
+            "vihr" -> R.drawable.vihr
+            else -> R.drawable.ic_launcher_foreground
+        })
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view, parent, false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_party_list, parent, false)
+        return ViewHolder(view)
     }
 
 
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val partyName: TextView = itemView.findViewById(R.id.party_name)
+        val partyLogo: ImageView = itemView.findViewById(R.id.party_logo)
+    }
 }
+
+
+
