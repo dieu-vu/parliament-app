51b867d06d77b9b6d80e22c145e359bcd460d46d
Dieu Vu <dieu.vu@metropolia.fi>
2021-10-10 11:48:52 +0300

    implemented Work Manager to refresh data every 6h

--- app/src/main/java/com/example/parliamentmemberapp/MyApp.kt
+++ app/src/main/java/com/example/parliamentmemberapp/MyApp.kt
@@ -2,14 +2,59 @@ package com.example.parliamentmemberapp
 
 import android.app.Application
 import android.content.Context
+import android.os.Build
+import androidx.work.*
+import com.example.parliamentmemberapp.work.RefreshDataWorker
+import com.example.parliamentmemberapp.work.RefreshDataWorker.Companion.WORK_NAME
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
 class MyApp : Application() {
+
+    //Make sure that the app launch is fast
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
     companion object {
         lateinit var appContext: Context
     }
 
+
     override fun onCreate() {
         super.onCreate()
         appContext = applicationContext
+        delayedInit()
+    }
+
+    private fun delayedInit(){
+        applicationScope.launch {
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork(){
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED) //UNMETER: User wont be charged for the network request
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true) //when the user isn't actively using the device
+                }
+            }.build()
+
+        //refresh data every six hours with the constraints
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(
+            6,
+            TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
     }
 }
---
+++ app/src/main/java/com/example/parliamentmemberapp/work/RefreshDataWork.kt
@@ -0,0 +1,27 @@
+package com.example.parliamentmemberapp.work
+
+import android.content.Context
+import android.service.voice.AlwaysOnHotwordDetector
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.parliamentmemberapp.data.MemberDatabase
+import com.example.parliamentmemberapp.repository.MemberDataRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+        CoroutineWorker(appContext, params) {
+
+        companion object{
+                const val WORK_NAME = "RefreshDataWorker"
+        }
+        override suspend fun doWork(): Result  {
+                val database = MemberDatabase.getInstance(applicationContext)
+                val repository = MemberDataRepository(database)
+                try {
+                        repository.refreshDatabase()
+                        return Result.success()
+                } catch (exception: HttpException){
+                        return Result.retry()
+                }
+        }
+}
